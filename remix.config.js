c27490a94c30e8fb408ae2cf606678528e91384b
DamilareDremel <dremelerablogdeb@gmail.com>
2025-06-18 12:47:47 +0100

    remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,9 @@
 import path from "path";
 
 export default {
-  future: {
-    v2_routeConvention: true,
-  },
-  ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
+  serverBuildPath: "netlify/functions/server.js",
   publicPath: "/build/",
-  serverBuildPath: "build/index.js",
-  serverModuleFormat: "esm",
-  devServerBroadcastDelay: 0,
+  serverModuleFormat: "esm"
 };
